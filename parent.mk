190015c2682e8c08251ffa0fb61525759bc61744
drathveloper <alberto.hdzhdz@gmail.com>
2025-11-03 20:56:46 +0100

    removed unnecessary slashes

--- parent.mk
+++ parent.mk
@@ -1,7 +1,7 @@
 PARENT_DIR := $(dir $(abspath $(lastword $(MAKEFILE_LIST))))
 
-include $(PARENT_DIR)/makefiles/service.mk
-include $(PARENT_DIR)/makefiles/commons.mk
-include $(PARENT_DIR)/makefiles/grpc.mk
-include $(PARENT_DIR)/makefiles/mock.mk
-include $(PARENT_DIR)/makefiles/lint.mk
+include $(PARENT_DIR)makefiles/service.mk
+include $(PARENT_DIR)makefiles/commons.mk
+include $(PARENT_DIR)makefiles/grpc.mk
+include $(PARENT_DIR)makefiles/mock.mk
+include $(PARENT_DIR)makefiles/lint.mk
