7a74539709fc13975dde206d2e5264da9acd9f9d
drathveloper <alberto.hdzhdz@gmail.com>
2025-11-03 20:44:55 +0100

    fix relative routes in parent.mk

--- parent.mk
+++ parent.mk
@@ -1,5 +1,7 @@
-include ./makefiles/service.mk
-include ./makefiles/commons.mk
-include ./makefiles/grpc.mk
-include ./makefiles/mock.mk
-include ./makefiles/lint.mk
+PARENT_DIR := $(dir $(abspath $(lastword $(MAKEFILE_LIST))))
+
+include $(PARENT_DIR)/makefiles/service.mk
+include $(PARENT_DIR)/makefiles/commons.mk
+include $(PARENT_DIR)/makefiles/grpc.mk
+include $(PARENT_DIR)/makefiles/mock.mk
+include $(PARENT_DIR)/makefiles/lint.mk
